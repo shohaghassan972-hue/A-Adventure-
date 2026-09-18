8a54841e5fcc209840bf2781bbb482fd773edf1b
shohaghassan972-hue <shohaghassan972@gmail.com>
2026-09-18 17:02:44 +0600

    Add import for MotionEvent in MainActivity

--- app/src/main/java/com/aadventure/app/MainActivity.java
+++ app/src/main/java/com/aadventure/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.aadventure.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
