3ccafe612e4a8816e8b149b6005264783a065fba
shohaghassan972-hue <shohaghassan972@gmail.com>
2026-09-18 14:42:08 +0600

    Refactor MainActivity to use FrameLayout for views

--- app/src/main/java/com/aadventure/app/MainActivity.java
+++ app/src/main/java/com/aadventure/app/MainActivity.java
@@ -2,11 +2,27 @@ package com.aadventure.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new WorldView(this));
+
+        FrameLayout root = new FrameLayout(this);
+
+        WorldView worldView = new WorldView(this);
+        root.addView(worldView, new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        ));
+
+        JoystickOverlay joystick = new JoystickOverlay(this, worldView.getRenderer());
+        root.addView(joystick, new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        ));
+
+        setContentView(root);
     }
 }
